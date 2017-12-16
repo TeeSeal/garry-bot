c2d67c7c1560dbe7ecdd03adc53d9f6c3bbe2acd
TeeSeal <alex.theoddor@gmail.com>
2017-12-16 16:02:06 +0200

    Add generic server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('POST request received.');
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
