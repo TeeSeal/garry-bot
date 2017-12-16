283d0d5a10f44117e2a3fbb9c0dec7c323c639ff
TeeSeal <alex.theoddor@gmail.com>
2017-12-16 16:23:19 +0200

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hi there'));
 app.post('/', (req, res) => {
     console.log(req.body);
     res.send('POST request received.');
