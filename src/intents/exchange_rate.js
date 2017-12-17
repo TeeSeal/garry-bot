7234637dddbfc5a97561175e1c6b9e5294117c0a
TeeSeal <alex.theoddor@gmail.com>
2017-12-17 09:14:07 +0200

    Add exchange rates

---
+++ src/intents/exchange_rate.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+
+const Intent = require('../struct/Intent.js');
+
+class ExchangeRateIntent extends Intent {
+    constructor(handler) {
+        super(handler, 'exchange_rate');
+    }
+
+    exec(data, res) {
+        const { from, to, amount } = data.params;
+
+        axios.get('https://bnm.md/en/official_exchange_rates?date=17.12.2017').then(r => {
+            xml2js.parseString(r.data, (err, p) => {
+                if (err) throw err;
+                const rates = p.ValCurs.Valute.map(v => [v.CharCode[0], parseFloat(v.Value[0])]);
+
+                if (!to) return res.addMessage(rates.map(rate => rate.join(': ')).join('\n')).send();
+                const toRate = rates.find(rate => rate[0] === to)[1];
+                const fromRate = from && from !== 'MDL' ? rates.find(rate => rate[0] === from)[1] : 1;
+                const amnt = parseInt(amount) || 1;
+
+                return res.addMessage(`${amnt} ${to} is currently ${toRate / fromRate * amnt} ${from}`).send();
+            });
+        });
+    }
+}
+
+module.exports = ExchangeRateIntent;
--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-const chrono = require('chrono-node');
-console.log(chrono.parseDate('12.10.2017'));
+const axios = require('axios');
+const xml2js = require('xml2js');
+axios.get('https://bnm.md/en/official_exchange_rates?date=17.12.2017').then(res => {
+    xml2js.parseString(res.data, (err, p) => {
+        if (err) throw err;
+        console.log(p.ValCurs.Valute.map(v => [v.CharCode[0], v.Value[0]]));
+    });
+});
