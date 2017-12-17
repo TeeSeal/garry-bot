5eed9f64273805dad9ec2a7980006f07f5d3f92d
TeeSeal <alex.theoddor@gmail.com>
2017-12-17 13:38:36 +0200

    Fix categories again

--- src/intents/category_spendings.js
+++ src/intents/category_spendings.js
@@ -21,8 +21,10 @@ class CategorySpendingsIntent extends Intent {
         const account = this.client.bank.accounts.find('name', accountName);
         if (!account) return res.addMessage('Sorry, couldn\'t find such an account.').send();
 
-        const transactions = this.client.bank.filterTransactions({ fromDate, account, category });
-        const amount = transactions.reduce((am, tr) => am + tr.amount, 0).toFixed(2);
+        const transactions = this.client.bank.filterTransactions({ fromDate, account, category })
+            .filter(t => t.amount < 0);
+
+        const amount = transactions.reduce((am, tr) => am + Math.abs(tr.amount), 0).toFixed(2);
         let response = `You spent ${amount} ${account.currencyCode} on ${category}`;
 
         if (fromDate) response += ` since ${fromDate}.`;
