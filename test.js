33fdba29d13a8cf8136baa14674ec6be0af99727
TeeSeal <alex.theoddor@gmail.com>
2017-12-17 09:16:08 +0200

    Add procfile

--- test.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-const xml2js = require('xml2js');
-axios.get('https://bnm.md/en/official_exchange_rates?date=17.12.2017').then(res => {
-    xml2js.parseString(res.data, (err, p) => {
-        if (err) throw err;
-        console.log(p.ValCurs.Valute.map(v => [v.CharCode[0], v.Value[0]]));
-    });
-});
